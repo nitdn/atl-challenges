d5a82fe1cf292c3d653990392fa2eb8245dac1ed
Nitesh Kumar Debnath <30214255+nitdn@users.noreply.github.com>
2025-09-21 18:59:34 +0530

    We build for the web now

--- flake.nix
+++ flake.nix
@@ -104,6 +104,7 @@
               jujutsu
               meld
               watchexec
+              trunk
             ];
 
             LD_LIBRARY_PATH = builtins.toString (pkgs.lib.makeLibraryPath buildInputs);
