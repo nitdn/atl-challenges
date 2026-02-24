a3c10f609d85d7930041b2a95b5d7ab9d4dd4ed4
Nitesh Kumar Debnath <nitkdnath@gmail.com>
2026-02-24 14:29:26 +0530

    minor snafu cleanup 2

--- sudoku.ml
+++ sudoku.ml
@@ -1,5 +1,3 @@
-module Ints = Set.Make (Int)
-
 let _problem_board_medium =
   [
   6; 5; 9; 0; 1; 0; 2; 8; 0;
@@ -33,20 +31,20 @@ let set_item board entry_index value =
     (fun index element -> if index = entry_index then value else element)
     board
 
+let diff list1 list2 =
+  List.filter (fun element -> not (List.mem element list2)) list1
+
 let valid_values board entry_index =
-  let numbers = Ints.of_list [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] in
+  let numbers = [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] in
   let filter_rows numbers =
-    let row =
-      Ints.of_list (List.filteri (fun idx _ -> idx / 9 = entry_index / 9) board)
-    in
-    Ints.diff numbers row
+    let row = List.filteri (fun idx _ -> idx / 9 = entry_index / 9) board in
+    diff numbers row
   in
   let filter_columns numbers =
     let column =
-      Ints.of_list
-        (List.filteri (fun idx _ -> idx mod 9 = entry_index mod 9) board)
+      List.filteri (fun idx _ -> idx mod 9 = entry_index mod 9) board
     in
-    Ints.diff numbers column
+    diff numbers column
   in
   let filter_boxes numbers =
     let get_box idx =
@@ -55,29 +53,28 @@ let valid_values board entry_index =
       (row / 3 * 3) + (column / 3)
     in
     let box =
-      Ints.of_list
-        (List.filteri (fun idx _ -> get_box idx = get_box entry_index) board)
+      List.filteri (fun idx _ -> get_box idx = get_box entry_index) board
     in
-    Ints.diff numbers box
+    diff numbers box
   in
   (* Grabs a list of valid values *)
-  numbers |> filter_rows |> filter_columns |> filter_boxes |> Ints.to_list
+  numbers |> filter_rows |> filter_columns |> filter_boxes
 
 let rec fill board =
+  let rec try_entry board idx = function
+    | [] -> None (*Oh god we are cooked*)
+    | curr :: rest -> (
+        let board = set_item board idx curr in
+        match fill board with
+        | Some _ as pass -> pass
+        | _ -> try_entry board idx rest)
+  in
   (* Find empty cell *)
   match List.find_index (fun value -> value = 0) board with
   (* Filled *)
   | None -> Some board
   | Some idx ->
       (* i would like to call this line recursively actually *)
-      let rec try_entry board idx = function
-        | [] -> None (*Oh god we are cooked*)
-        | curr :: rest -> (
-            let board = set_item board idx curr in
-            match fill board with
-            | Some _ as pass -> pass
-            | _ -> try_entry board idx rest)
-      in
       try_entry board idx (valid_values board idx)
 
 let get_as_string value = if value > 0 then string_of_int value else "."
@@ -95,7 +92,7 @@ let print_board board =
     board
 
 let solved_board =
-  match fill problem_board with Some board -> board | None -> failwith "bad"
+  match fill problem_board with Some board -> board | None -> failwith "bad board"
 
 let () =
   print_endline "Problem board:";
